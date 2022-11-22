2df168adb56270de202b101554b54a83790eea5f
Sean <joseanmanuel@gmail.com>
2022-11-23 10:04:13 +1300

    Formatted test.py Updated README with installing requirements.txt Created requirements.txt Updated main.py to account for 24 hour format and calculate differences

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Date: 09/11/2022
 # Sources: https://pynative.com/python-get-time-difference/#:~:text=To%20get%20the%20difference%20between%20two%2Dtime%2C%20subtract%20time1%20from,time%20to%20the%20microsecond%20resolution.&text=To%20get%20a%20time%20difference%20in%20seconds%2C%20use%20the%20timedelta.
 
-import numpy as np
+# import numpy as np
 import pandas as pd
 from datetime import datetime
 
@@ -12,30 +12,53 @@ def loop_dp(filtered_df):
     """Loops through DF and conducts string manipulation to acquire totals for rooms and for organizations"""
     df = filtered_df.reset_index()
 
+    totals_user_id = {}
+    totals_room_num = {}
+    curr_open = None
+
     for _, row in df.iterrows():
 
         # To get room_num
         description_list = " ".join(row[5].split(" >")).split(" ")[:4]
         del description_list[1]
-        print(description_list)
-        continue
-        # To get user_ID
 
         # To get totals
         ##
         timed = row[1].split(" ")
         timed[3] = ''.join(timed[3].split("."))
 
+        # Account for improper formatting of document read by adding zero if missing for the HH
         if timed[2][2] != ":":
             timed[2] = "0"+timed[2]
 
+        # Convert to 24-hour format
+        if timed[3] == 'pm':
+            if int(timed[2][0:2]) != 12:
+                time_24 = str(12 + int(timed[2][0:2]))
+                timed[2] = time_24 + timed[2][2:]
+
+        # Remove unnecessary am/pm from list
+        timed.pop()
         timed = ' '.join(timed)
-        parsed_time = datetime.strptime(timed, '%d/%m/%Y %H:%M:%S %p')
-        ##
+
+        if curr_open is None:
+            curr_open = datetime.strptime(timed, '%d/%m/%Y %H:%M:%S')
+
+        else:
+            difference = datetime.strptime(timed, '%d/%m/%Y %H:%M:%S') - curr_open
+            print(difference)
+            curr_open = None
+
+
+        # To get user_ID
+
+        # try:
+        #     totals_user_id[] =
+
 
 
 def main():
-    df = pd.read_excel("C:/Users/user/Documents/PatRep260922206883.xls", header=None)
+    df = pd.read_excel("C:/Users/Sean/Downloads/PatRep260922206883.xls", header=None)
     filtered_df = df[df[5].str.contains('Open|Close') == True]
     filtered_df = filtered_df.dropna(axis=1)
 
